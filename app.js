e22297eedee7be61f05f027623164f79a78392fe
David Lenh <dlenh425@gmail.com>
2022-08-09 12:49:12 -0700

    Added sessions and passport middleware

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const express = require("express")
 const dotenv = require("dotenv")
 const morgan = require("morgan")
 const exphbs = require("express-handlebars")
+const passport = require("passport")
+const session = require("express-session")
 const connectDB = require("./config/db")
 
 // load config
 dotenv.config({ path: "./config/config.env" })
+
+// passport config
+require("./config/passport")(passport)
 
 connectDB()
 
@@ -25,6 +30,19 @@ app.engine(".hbs", exphbs.engine({
     })
 );
 app.set("view engine", ".hbs");
+
+// sessions
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false
+    })
+)
+
+// passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 // static folder
 app.use(express.static(path.join(__dirname, "public")))
