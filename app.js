e8904dda90227b0af345bff7562cfad99781b387
David Lenh <dlenh425@gmail.com>
2022-08-10 10:12:28 -0700

    Added auth route to app.js

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ app.use(express.static(path.join(__dirname, "public")))
 
 // routes
 app.use("/", require("./routes/index"))
+app.use("/", require("./routes/auth"))
  
 const PORT = process.env.PORT || 3000
 
