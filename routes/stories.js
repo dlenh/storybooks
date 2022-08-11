f5e857bdcd85e75780fa92b4b01ef31926190926
David Lenh <dlenh425@gmail.com>
2022-08-11 15:27:08 -0700

    Added route to show all stories

--- routes/stories.js
+++ routes/stories.js
@@ -21,6 +21,24 @@ router.post("/", ensureAuth, async (req, res) => {
         console.error(err)
         res.render("error/500")
     }
+})
+
+// @desc show all stories
+// @route GET /stories
+router.get("/", ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({ status: "public" })
+            .populate("user")
+            .sort({ createdAt: "desc" })
+            .lean() // mongoose object to json object
+
+        res.render("stories/index", {
+            stories,
+        })
+    } catch (err) {
+        console.error(err)
+        res.render("error/500")
+    }
  })
 
 module.exports = router
