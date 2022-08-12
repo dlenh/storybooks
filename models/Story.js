321836f84e182c85e5e5160e7e36a617fe08e4b8
David Lenh <dlenh425@gmail.com>
2022-08-12 13:58:37 -0700

    Making user required if not app will break

--- models/Story.js
+++ models/Story.js
@@ -17,7 +17,8 @@ const StorySchema = new mongoose.Schema({
     },
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
+        ref: "User",
+        required: true // app will break if the user is not present
     },
     createdAt: {
         type: Date,
