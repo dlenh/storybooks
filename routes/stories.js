c766a81d4b45cfd2a13ac96aa2b50b02f49763d0
David Lenh <dlenh425@gmail.com>
2022-08-16 13:30:53 -0700

    Added page to view single story

--- routes/stories.js
+++ routes/stories.js
@@ -43,7 +43,7 @@ router.get("/", ensureAuth, async (req, res) => {
 
 // @desc show single story
 // @route GET /stories/:id
-router.get("/add", ensureAuth, async (req, res) => {
+router.get("/:id", ensureAuth, async (req, res) => {
     try {
         let story = await Story.findById(req.params.id)
             .populate("user")
@@ -123,6 +123,26 @@ router.delete("/:id", ensureAuth, async (req, res) => {
     } catch (err) {
         console.error(err)
         return res.render("error/500")
+    }
+})
+
+// @desc user stories
+// @route GET /stories/user/:userId
+router.get("/user/:userId", ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({
+            user: req.params.userId,
+            status: "public"
+        })
+        .populate("user")
+        .lean()
+
+        res.render("stories/index", {
+            stories
+        })
+    } catch (err) {
+        console.error(err)
+        res.render("error/500")
     }
 })
 
